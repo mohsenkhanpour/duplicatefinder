6c7759c7fa87e351d44b2826c4545e59c2765ba7
mohsenkhanpour <mohsenkhanpour@gmail.com>
2019-07-18 22:52:44 +0430

    update

--- test.py
+++ test.py
@@ -11,11 +11,25 @@ client = TelegramClient('DuplicateFinder', api_id, api_hash)
 client.start()
 print("Connected as:", client.get_me().username)
 
-messages_dailystrips = client.get_messages("dailystrips", limit=5)
-for message in messages_dailystrips:
-        print(message.id, message.message)
+""" messages_ds_get = client.get_messages("dailystrips", limit=5)
+for message in messages_ds_get:
+        print(message.id, message.message) """
 
-messages_dailystrips2 = client.iter_messages("dailystrips", limit=1000)
+messages_ds_iter = client.iter_messages("dailystrips", limit=10000000)
 
-for message in messages_dailystrips:
-        print(message.id, message.message)
+unique_messages = []
+duplicate_messages = []
+for message in messages_ds_iter:
+    if message.message in unique_messages:
+        duplicate_messages.append(message.message)
+    else:
+        unique_messages.append(message.message)
+
+""" print(unique_messages) """
+
+def save_to_file(message):
+        print(str(message.encode("unicode_escape"),"utf-8"), "\n", file=open("output.txt", "a")) 
+        
+for message in duplicate_messages:
+        save_to_file(message)
+        print(message)
