8b25a9fa21460567c2ed788e7d840710853817f0
mohsenkhanpour <mohsenkhanpour@gmail.com>
2020-02-29 16:40:41 +0330

    update

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ print("Connected as:", client.get_me().username)
 for message in messages_ds_get:
         print(message.id, message.message) """
 
-messages_ds_iter = client.iter_messages("dailystrips", limit=10000000000000000)
+messages_ds_iter = client.iter_messages("dailystrips", limit=1000000, min_id=307522)
 
 unique_messages = []
 duplicate_messages = []
@@ -25,11 +25,12 @@ for message in messages_ds_iter:
     if message.message in unique_messages:
         duplicate_messages.append(message.message)
         duplicate_ids.append(message.id)
-        print("duplicate found:" + message.id)
+        print("duplicate found:", message.id)
     else:
         unique_messages.append(message.message)
 
-print(unique_messages, file=open("output_unique.txt", "w"))
+print(duplicate_ids, file=open("output_ids.txt", "w"))
+
 
 def save_to_file(message):
     print(str(message.encode("unicode_escape"), "utf-8"),
@@ -38,10 +39,12 @@ def save_to_file(message):
 
 for message in duplicate_messages:
     save_to_file(message)
-    # print(message)
+
 print(duplicate_ids)
 
-
-print(duplicate_ids, file=open("output_ids.txt", "w"))
-client.delete_messages("dailystrips", duplicate_ids)
+for message in duplicate_ids:
+    client.delete_messages("dailystrips", message)
+    print("duplicate removed:", message)
+    
+#client.delete_messages("dailystrips", duplicate_ids)
 print("duplicates removed")
