9eede23faf34f574d593274b503aa27b183b790d
mohsenkhanpour <mohsenkhanpour@gmail.com>
2020-04-19 14:22:45 +0430

    cleanup

--- df.py
+++ df.py
@@ -1,23 +1,23 @@
 import os
 from telethon import TelegramClient, events, sync
 from dotenv import load_dotenv
-load_dotenv()
+
 
 # Client configuration
+load_dotenv()
+
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 chat_id = os.getenv("CHAT_ID")
 
+# Client connection
 client = TelegramClient('DuplicateFinder', api_id, api_hash)
 client.start()
 print("Connected as:", client.get_me().username)
 
-""" messages_ds_get = client.get_messages("dailystrips", limit=5)
-for message in messages_ds_get:
-        print(message.id, message.message) """
 
-messages_ds_iter = client.iter_messages(
-    "dailystrips", limit=1000000, min_id=307522)
+# Iterate over messages
+messages_ds_iter = client.iter_messages(chat_id, limit=1000000, min_id=307522)
 
 unique_messages = []
 duplicate_messages = []
@@ -27,7 +27,7 @@ for message in messages_ds_iter:
     if message.message in unique_messages:
         duplicate_messages.append(message.message)
         duplicate_ids.append(message.id)
-        print("duplicate found:", message.id)
+        print("Duplicate found:", message.id,":", message.message)
     else:
         unique_messages.append(message.message)
 
@@ -45,8 +45,7 @@ for message in duplicate_messages:
 print(duplicate_ids)
 
 for message in duplicate_ids:
-    client.delete_messages("dailystrips", message)
-    print("duplicate removed:", message)
+    client.delete_messages(chat_id, message)
+    print("Duplicate removed:", message)
 
-#client.delete_messages("dailystrips", duplicate_ids)
-print("duplicates removed")
+print("All duplicates removed.")
